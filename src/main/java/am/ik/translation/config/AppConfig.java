32e7832ddd9278d45090e7c86787c8276002b889
Toshiaki Maki <makingx@gmail.com>
2024-05-27 16:38:04 +0900

    Expose accesslog key-values

--- src/main/java/am/ik/translation/config/AppConfig.java
+++ src/main/java/am/ik/translation/config/AppConfig.java
@@ -3,6 +3,7 @@ package am.ik.translation.config;
 import java.util.Set;
 
 import am.ik.accesslogger.AccessLogger;
+import am.ik.accesslogger.AccessLoggerBuilder;
 import am.ik.spring.http.client.RetryableClientHttpRequestInterceptor;
 import am.ik.translation.github.GithubProps;
 import am.ik.webhook.spring.WebhookVerifierRequestBodyAdvice;
@@ -25,11 +26,11 @@ public class AppConfig {
 
 	@Bean
 	public AccessLogger accessLogger() {
-		return new AccessLogger(httpExchange -> {
+		return AccessLoggerBuilder.accessLogger().filter(httpExchange -> {
 			String uri = httpExchange.getRequest().getUri().getPath();
 			return uri != null && !(uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator")
 					|| uri.startsWith("/cloudfoundryapplication"));
-		});
+		}).addKeyValues(true).build();
 	}
 
 	@Bean
