947131c5d42913b7a20df16fbf9cc48a57703013
Toshiaki Maki <makingx@gmail.com>
2024-12-04 22:15:16 +0900

    Create MicrometerConfig.java

---
+++ src/main/java/am/ik/translation/config/MicrometerConfig.java
@@ -0,0 +1,19 @@
+package am.ik.translation.config;
+
+import io.micrometer.core.instrument.config.MeterFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class MicrometerConfig {
+
+	@Bean
+	public MeterFilter meterFilter() {
+		return MeterFilter.deny(id -> {
+			String uri = id.getTag("uri");
+			return uri != null && (uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator")
+					|| uri.startsWith("/_static"));
+		});
+	}
+
+}
