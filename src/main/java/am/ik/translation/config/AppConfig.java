710e7834ebe0b6ef7d32d805407dddb59c569633
Toshiaki Maki <makingx@gmail.com>
2025-09-10 16:35:30 +0900

    Tweak backoff

--- src/main/java/am/ik/translation/config/AppConfig.java
+++ src/main/java/am/ik/translation/config/AppConfig.java
@@ -1,19 +1,17 @@
 package am.ik.translation.config;
 
-import java.util.Set;
-
 import am.ik.spring.http.client.RetryableClientHttpRequestInterceptor;
 import am.ik.translation.github.GithubProps;
 import am.ik.webhook.spring.WebhookVerifierRequestBodyAdvice;
-import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
-
+import java.util.Set;
 import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.support.ContextPropagatingTaskDecorator;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.util.backoff.FixedBackOff;
+import org.springframework.util.backoff.ExponentialBackOff;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @Configuration(proxyBeanMethods = false)
 public class AppConfig {
@@ -27,12 +25,24 @@ public class AppConfig {
 	@Bean
 	public RestClientCustomizer restClientCustomizer(
 			LogbookClientHttpRequestInterceptor logbookClientHttpRequestInterceptor) {
+		ExponentialBackOff backOff = new ExponentialBackOff();
+		backOff.setInitialInterval(3_000L);
+		backOff.setMaxInterval(60_000L);
+		backOff.setMultiplier(2);
 		return builder -> builder.requestFactory(new SimpleClientHttpRequestFactory())
 			.requestInterceptor(logbookClientHttpRequestInterceptor)
-			.requestInterceptor(new RetryableClientHttpRequestInterceptor(new FixedBackOff(2_000, 2),
-					options -> options.sensitiveHeaders(Set.of(HttpHeaders.AUTHORIZATION.toLowerCase(),
-							HttpHeaders.PROXY_AUTHENTICATE.toLowerCase(), HttpHeaders.COOKIE.toLowerCase(),
-							HttpHeaders.SET_COOKIE.toLowerCase(), "x-amz-security-token"))));
+			.requestInterceptor(new RetryableClientHttpRequestInterceptor(backOff, Set.of( //
+					408 /* Request Timeout */, //
+					425 /* Too Early */, //
+					429 /* Too Many Requests */, //
+					500 /* Internal Server Error */, //
+					502 /* Bad Gateway */, //
+					503 /* Service Unavailable */, //
+					504 /* Gateway Timeout */, //
+					529 /* Overloaded Error */
+			), options -> options.sensitiveHeaders(Set.of(HttpHeaders.AUTHORIZATION.toLowerCase(),
+					HttpHeaders.PROXY_AUTHENTICATE.toLowerCase(), HttpHeaders.COOKIE.toLowerCase(),
+					HttpHeaders.SET_COOKIE.toLowerCase(), "x-amz-security-token"))));
 	}
 
 	@Bean
